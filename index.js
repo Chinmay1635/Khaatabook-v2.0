2b2ad68ed75a0cfaab18958d35deeae39a96a62a
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-07-07 12:08:36 +0530

    ok

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var invalid;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
